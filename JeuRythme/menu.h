e0025e936a49ee0ee49cc612ed7919c54a91ac6b
alexfaure <82882326+alexfaure@users.noreply.github.com>
2021-05-24 15:57:27 +0200

    comments

--- JeuRythme/menu.h
+++ JeuRythme/menu.h
@@ -4,11 +4,11 @@
 
 class Menu {  // Faure
 public:
-	sf::RenderWindow *renderWindow_;
-	sf::Text text[5];
-	sf::RectangleShape rectangle[4];
+	sf::RenderWindow *renderWindow_; // Fenêtre d'affichage 
+	sf::Text text[5]; // Les différents textes affichés dans le menu
+	sf::RectangleShape rectangle[4]; // Les différents rectangles servant de hitbox pour les cliques dans le menu 
 public:
-	Menu(sf::RenderWindow& window, sf::Font& font);
-	void draw();
+	Menu(sf::RenderWindow& window, sf::Font& font); // Constructeur du menu
+	void draw(); // Fonction qui dessine le menu 
 };
 #endif
