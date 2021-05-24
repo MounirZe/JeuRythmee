dc602151c76d2ab70ff3b9d2e2c614981f22ce96
MounirZe <sukiu59170@hotmail.com>
2021-05-24 16:12:54 +0200

    Update menu.h

--- JeuRythme/menu.h
+++ JeuRythme/menu.h
@@ -7,8 +7,13 @@ public:
 	sf::RenderWindow *renderWindow_; // Fenêtre d'affichage 
 	sf::Text text[5]; // Les différents textes affichés dans le menu
 	sf::RectangleShape rectangle[4]; // Les différents rectangles servant de hitbox pour les cliques dans le menu 
-public:
-	Menu(sf::RenderWindow& window, sf::Font& font); // Constructeur du menu
+public:	
+	/**
+	Constructeur de Menu
+	@param window Fenetre où le menu va être affiché
+	@param font Police d'écriture utilisée
+	**/
+	Menu(sf::RenderWindow& window, sf::Font& font); 
 	void draw(); // Fonction qui dessine le menu 
 };
 #endif
